576b6771fcb5854a33821cd1c37c94a84d2376f7
Seb <sdieunidou@users.noreply.github.com>
2020-08-07 11:53:19 +0200

    Update movie_record_ui.js

--- manager/movie_record_ui.js
+++ manager/movie_record_ui.js
@@ -10,6 +10,8 @@
 
 $(document).ready(function() {
   setTimeout(function() {
+    $("<style type='text/css'> .swal2-popup .swal2-title { display: none; } </style>").appendTo("head");
+
     $('#game-console').css('overflow-x', 'hidden');
     $('#app-container > div.page-container > div.page-content-wrapper > div > div > div:nth-child(3) > div > div.col-md-4.col-sm-12 > div:nth-child(3) > div.portlet-body > table > tbody > tr:nth-child(2)').remove();
     $('#app-container > div.page-container > div.page-content-wrapper > div > div > div.page-head > div > h1 > span, #app-container > div.page-container > div.page-content-wrapper > div > div > div:nth-child(3) > div > div.col-md-4.col-sm-12 > div:nth-child(3) > div.portlet-body > table > tbody > tr:nth-child(1) > td.text-right > small').text('GAME-FR-1337.MTXSERV.COM:27510');
